2af71bebf474bee117077b336c287d03ba9d0577
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-08 07:32:20 +0530

    update the post endpoint

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -54,3 +54,30 @@ router.post('/', async(req, res) =>{
     }
 })
 
+//update an exisiting post
+router.put('/:id', async(req, res) =>{
+    try{
+        const post = await Post.findById(req.params.id);
+        if (!post){
+            return res.status(404).json({
+                message: "Post not found"
+            })
+        }
+        post.title = req.body.title || post.title
+        post.content = req.body.content || post.content
+        post.category = req.body.category || post.category
+        post.author = req.body.author || post.author
+        post.image = req.body.image || post.image
+        post.updatedAt = Date.now()
+
+        const updatedPost = await post.save()
+        
+        res.status(200).json(updatedPost)
+
+    }
+    catch(error){
+        res.status(500).json({
+            message: "Internal Server error"
+        })
+    }
+})
