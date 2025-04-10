366283c954ee572741f02e042c5a7583452cbdae
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-10 11:41:41 +0530

    React router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 
 import PostList from './pages/PostList';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <PostList/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<PostList/>}>
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
