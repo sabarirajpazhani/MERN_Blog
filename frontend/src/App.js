6fffc6d93e7295b243f25593398c3b1a5d320461
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-10 12:03:43 +0530

    Creating header, footer , PostList and PostDescription

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 
 import PostList from './pages/PostList';
+import PostDetails from './pages/PostDetails'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Router>
         <Routes>
-          <Route path='/' element={<PostList/>}>
-
-          </Route>
+          <Route path='/' element={<PostList/>}/>
+          <Route path='/posts/:id' element={<PostDetails/>}/>
         </Routes>
       </Router>
+      <Footer/>
     </div>
   );
 }
