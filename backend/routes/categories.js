19841f879a4514d4836d8908574acc300ca23a9e
sabarirajpazhani <sabarirajpazhani@gmail.com>
2025-04-15 20:01:11 +0530

    Rotuer: Get Single category by ID

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -14,3 +14,22 @@ router.get('/', async(req, res)=>{
         })
     }
 })
+
+//Get Single category by ID
+router.get('/:id', async(req, res) =>{
+    try{
+        const category = await Category.findById(req.params.id);
+        if (!category){
+            return res.status(404).json({
+                message: "Category not found"
+            })
+        }
+        res.status(200).json(category)
+    }
+    catch(error){
+        res.status(500).json({
+            message: "Internal server Error"
+        })
+    }
+})
+
